961a1ff46b6f7d439cb4f5f411b236f7bc4c9eda
Ben Burton <sistematic3@yahoo.co.uk>
2016-01-10 00:20:08 +0000

    Adding new tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,14 +11,42 @@ module.exports = function (grunt) {
       main: {
         src: ['src/**/*.tpl.html'],
         dest: 'src/osbNewsFeedReaderTemplate.js'
+      },
+      sass: {
+        options: {
+          sourceMap: true
+        },
+        dist: {
+          files: {
+            'src/css/osbNewsFeedReader.css': 'src/scss/osbNewsFeedReader.scss'
+          }
+        }
+      },
+      watch: {
+        templates: {
+          files: ['src/**/*.html'],
+          tasks: ['html2js'],
+          options: {
+            spawn: false
+          }
+        },
+        styles: {
+          files: ['src/scss/**/*.scss'],
+          tasks: ['sass'],
+          options: {
+            spawn: false
+          }
+        }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-html2js');
   grunt.loadNpmTasks('grunt-serve');
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['serve']);
   grunt.registerTask('template', ['html2js']);
+  grunt.registerTask('style', ['sass']);
 };
